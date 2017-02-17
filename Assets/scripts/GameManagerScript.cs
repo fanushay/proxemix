29a0e646efa846ac39253b4d781fabe53ef0ef44
Quinn <qmm25@drexel.edu>
2017-02-17 15:43:02 -0500

    addiing gamemanager and introvert scriptz

---
+++ Assets/scripts/GameManagerScript.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+
+
+    //Personal Space and Social Space Minimum and Maximum Values
+    [Header("Introverts")]
+    public GameObject introvertPrefab;
+    //number of Introverts
+    //public int[] NumberOfIntros;
+    public int numIntros;
+    //introveert personal space
+    public int introPersMin;
+    public int introPersMax;
+    //introvert social space
+    public int introSocMin;
+    public int introSocMax;
+
+    [Header("Extroverts")]
+    public GameObject extrovertPrefab;
+    //number of Extroverts
+    public int numExtros;
+    //extrovert personal space
+    public int extroPersMin;
+    public int extroPersMax;
+    //extrovert social space
+    public int extroSocMin;
+    public int extroSocMax;
+
+    [Header("Friends A")]
+    public GameObject friendAPrefab;
+    //number of Friendss
+    public int numFriendsA;
+    //friends personal space
+    public int friendAPersMin;
+    public int friendAPersMax;
+    //friends social space
+    public int friendASocMin;
+    public int friendASocMax;
+
+    [Header("Friends B")]
+    public GameObject friendBPrefab;
+    //number of Friendss
+    public int numFriendsB;
+    //friends personal space
+    public int friendBPersMin;
+    public int friendBPersMax;
+    //friends social space
+    public int friendBSocMin;
+    public int friendBSocMax;
+
+    [Header("Couples")]
+    public GameObject couplePrefab;
+    //number of Couples
+    public int numCouples;
+    //couples personal space
+    public int couplePersMin;
+    public int couplePersMax;
+    //couples social space
+    public int coupleSocMin;
+    public int coupleSocMax;
+
+
+
+    
+
+    
+
+    
+    
+
+    //Create Coordinates For each Character
+    
+    
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //for (int i = 0; i < numIntros; i++)
+        //{
+            
+            Instantiate(introvertPrefab, new Vector2(1,1), transform.rotation);
+            Instantiate(introvertPrefab, new Vector2(1, 1), transform.rotation);
+            Instantiate(introvertPrefab, new Vector2(8, 1), transform.rotation);
+            Instantiate(introvertPrefab, new Vector2(12, 2), transform.rotation);
+            Instantiate(introvertPrefab, new Vector2(16, 1), transform.rotation);
+        //}
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+      
+          
+       
+
+    }
+}
---
+++ Assets/scripts/IntrovertScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntrovertScript : MonoBehaviour {
+
+    private int inPersSpace;
+    private int inSocSpace;
+    private bool isComfy;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("check1");
+        inPersSpace++;
+        inSocSpace++;
+    }
+
+
+
+   
+
+    // Use this for initialization
+    void Start () {
+	
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+    if (inPersSpace > 0 || inSocSpace > 1)
+        {
+            //Color.red;
+            isComfy = false;
+        }
+    else if (inSocSpace > 1)
+        {
+            //Color.red;
+            isComfy = false;
+        }
+
+	}
+}
