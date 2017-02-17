097b013489b5058834d5dc90d7328e2687540a7d
Lewis Ellis <lje34@drexel.edu>
2017-02-17 18:56:10 -0500

    added snap to grid and click and drag scripts

---
+++ Assets/scripts/snaptogrid.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class snaptogrid : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        transform.position = new Vector3(Mathf.Round(transform.position.x), Mathf.Round(transform.position.y), Mathf.Round(transform.position.z));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
