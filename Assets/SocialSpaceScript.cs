ef98781da42ad561b52b53a7f4d3a5a718a43673
Quinn <qmm25@drexel.edu>
2017-02-19 16:48:45 -0500

    i hope this works

---
+++ Assets/SocialSpaceScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SocialSpaceScript : MonoBehaviour
+{
+
+    //SpriteRenderer socSprite = GetComponent<SpriteRenderer>();
+    public Color unComfyColor = new Color();
+    public Color comfyColor = new Color();
+
+    private int numBreach = 0;
+
+
+    void ChangeColor(Color newColor)
+    {
+        SpriteRenderer socRender = GetComponent<SpriteRenderer>();
+        socRender.color = newColor;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        numBreach++;
+        Debug.Log(numBreach);
+
+
+        if (numBreach > 1)
+        {
+
+            Debug.Log("Too Many In Social Space!");
+            ChangeColor(unComfyColor);
+        }
+    }
+
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        numBreach--;
+        Debug.Log(numBreach);
+
+        if (numBreach > 1)
+        {
+
+            Debug.Log("Too Many In Social Space!");
+            ChangeColor(unComfyColor);
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
