7c0d82a22377d29e690b21ea6a43e3386d72a6d9
Anushay Furqan <anushay.furqan@gmail.com>
2017-02-20 18:57:17 -0500

    missing files

---
+++ Assets/PersonalSpaceScript.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonalSpaceScript : MonoBehaviour
+{
+	
+    //SpriteRenderer socSprite = GetComponent<SpriteRenderer>();
+    public Color unComfyColor = new Color();
+    public Color comfyColor = new Color();
+
+	public bool amIHappy;
+
+
+
+    private int numBreach = 0;
+
+	
+
+    void ChangeColor(Color newColor)
+    {
+        SpriteRenderer persRender = GetComponent<SpriteRenderer>();
+        persRender.color = newColor;
+    }
+	
+		
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        numBreach++;
+        Debug.Log(numBreach);
+
+
+        if (numBreach > 2)
+        {
+
+            Debug.Log("Too Many In Personal Space!");
+            ChangeColor(unComfyColor);
+			amIHappy = false;
+        }
+    }
+
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        numBreach--;
+        Debug.Log(numBreach);
+
+		if (numBreach > 2) {
+
+			Debug.Log ("Too Many In Personal Space!");
+			ChangeColor (unComfyColor);
+			amIHappy = false;
+		} 
+		else { 
+			ChangeColor (comfyColor);
+			amIHappy = true;
+		}
+    }
+
+    // Use this for initialization
+
+
+	void Start()
+	{
+	}
+
+	void Update()
+	{
+  
+
+    // Update is called once per frame
+    
+}
+    
+}
